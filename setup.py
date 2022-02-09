ab86367ea62e9e0b15ac27870a36dcbccc9d9548
Jesus <jrosalesg1500@alumno.ipn.mx>
2022-02-09 11:22:23 -0600

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Analisis de canciones para crear un modelo que permita identificar tendencias.',
+    author='JesusRosales',
+    license='MIT',
+)
